0f05f57f8f1668eed7274e276e205791494bbee9
aburkex24 <aburkex24@gmail.com>
2016-06-27 00:25:29 -0600

    updated navbar

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     });
 });
 
-$('.nav a').on('click', function(){
-    $('.btn-navbar').click(); //bootstrap 2.x
-    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+$(".nav a").on("click", function(){
+    $(".btn-navbar").click(); //bootstrap 2.x
+    $(".navbar-toggle").click() //bootstrap 3.x by Richard
 });
