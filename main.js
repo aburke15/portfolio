fd1c27ffcdfb6c80236a3b92dfd1e863e435c0db
Andre Burke <Andre Burke>
2017-04-15 17:44:14 -0600

    scroll animation change

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             var hash = this.hash;
             $("html, body").animate({
                 scrollTop: $(hash).offset().top
-            }, 600, function () {
+            }, 750, function () {
                 window.location.hash = hash;
             });
         }
